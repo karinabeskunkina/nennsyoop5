ecc05eb0909f729ebb531a961dcee648f263638e
Karina Lavrenieva <lkvlkvlkv2003@yandex.ru>
2022-03-29 23:33:36 +0300

    Добавьте файлы проекта.

---
+++ nennsyoop5.cpp
@@ -0,0 +1,23 @@
+﻿#include "storage.h"
+
+
+int main()
+{
+	string choice;
+	choice = "Yes";
+	while (choice == "Yes" or choice == "YES" or choice == "yes") {
+		int n, sum;
+		cin >> n;
+		double* arr;
+		arr = Array(n);
+		ArrayCompl(arr, n);
+		ArrayCout(arr, n);
+		StorageDelete(arr);
+		cout << "Do you want to continue working?" << endl;
+		string print;
+		cin >> print;
+		choice = print;
+	}
+	return 0;	
+}
+
---
+++ storage.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef CONDITIONS_H
+#define CONDITIONS_H
+
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <string>
+using namespace std;
+double* Array(int);
+void ArrayCompl(double*, int);
+void ArrayCout(double* , int);
+void StorageDelete(double*);
+#endif
